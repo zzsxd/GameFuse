f1fb7e936048ab7ecfdeb18f2ac6090871a8d919
zzsxd <nikitacapkov06@gmail.com>
2024-02-12 00:16:49 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#################################################
+#                 created by                    #
+#                     ZZS                       #
+#                     SBR                       #
+#################################################
+import flet as ft
+from frontend import main
+
+if __name__ == '__main__':
+    ft.app(target=frontend.main)
+    #view=ft.AppView.WEB_BROWSER
